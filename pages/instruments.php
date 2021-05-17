e53f403064a50e87a84eae4bbf1f6104e2673c5f
Cassimiroardo <edu27.cassimiro@gmail.com>
2021-05-16 22:24:40 -0300

    ajustando

--- pages/instruments.php
+++ pages/instruments.php
@@ -10,7 +10,7 @@
 <body>
     <div class="container">
         <header>
-            <a href="../index.php">
+            <a href="../index.html">
                 <h1>Voltar</h1>
             </a>
         </header>
